5cf6eabdc5690fb0d10d2bdf6846137865a40ff3
NwekeMaxwell <nwekemaxwell37@gmail.com>
2023-04-14 11:51:40 +0100

    update: new UI

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -3,8 +3,6 @@ import "./Intro.css";
 import Github from "../../img/github.png";
 import LinkedIn from "../../img/linkedin.png";
 import Instagram from "../../img/instagram.png";
-import Vector1 from "../../img/Vector1.png";
-import Vector2 from "../../img/Vector2.png";
 import boy from "../../img/boy.png";
 import thumbup from "../../img/thumbup.png";
 import Crown from "../../img/crown.png";
@@ -29,11 +27,12 @@ const Intro = () => {
           </span>
           <span>Nweke Maxwell</span>
           <span>
+            "A Passionate Frontend Developer with high level of experience in
+            web designing and development, producing the Quality work!!!",
+          </span>
+          <span>
             <Typed
               strings={[
-                "A Frontend Developer with high level of experience in web designing and development, producing the Quality work!!!",
-                "A passionate Web designer with an extreme creative mind🎇",
-                "An enthusiastic Coder 🤓🥳😇",
                 "You can switch to day/night mode by clicking the toggle button above 🌃☀️",
               ]}
               typeSpeed={50}
@@ -57,10 +56,11 @@ const Intro = () => {
           </a>
         </div>
       </div>
+
       <div className="i-right">
-        <img src={Vector1} alt="" />
-        <img src={Vector2} alt="" />
-        <img src={boy} alt="" />
+        <div className="i-avatar">
+          <img src={boy} alt="" />
+        </div>
 
         <motion.img
           initial={{ left: "-36%" }}
@@ -81,8 +81,8 @@ const Intro = () => {
         </motion.div>
 
         <motion.div
-          initial={{ top: "20rem", left: "9rem" }}
-          whileInView={{ left: "19.5%" }}
+          initial={{ top: "25rem", left: "9rem" }}
+          whileInView={{ left: "25.5%" }}
           transition={transition}
           className="i-floatingDiv2"
         >
