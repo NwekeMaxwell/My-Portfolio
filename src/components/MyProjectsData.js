428686c110aeabc2411e118dd30fe6af7ce18608
NwekeMaxwell <nwekemaxwell37@gmail.com>
2023-04-14 11:49:58 +0100

    feat: array of objs containing all projects

---
+++ src/components/MyProjectsData.js
@@ -0,0 +1,172 @@
+import Portfolio from "../img/portfolio.png";
+import Forkify from "../img/forkify.png";
+import Bankist from "../img/bankist.png";
+import ChefMax from "../img/chefMax.png";
+import Church from "../img/church.png";
+import Mapty from "../img/mapty.png";
+import MaxArt from "../img/MaxArt.png";
+import MiniBankist from "../img/miniBankist.png";
+import PigGame from "../img/pigGame.png";
+import Lendsqr from "../img/lendsqr.png";
+import Kitty from "../img/Rolodex.png";
+import GuessMyAge from "../img/guessmyage.png";
+
+const MyProjectsData = [
+  {
+    id: 1,
+    title: "Lendsqr",
+    image: Lendsqr,
+    link: "https://nweke-maxwell-lendsqr-fe-test.netlify.app",
+    github: "https://github.com/NwekeMaxwell/lendsqr-fe-test",
+    description:
+      "A static finance 4 page website accessing and showing over 500 user details from external Library / API.",
+    stack: ["TypeScript", "React"],
+    projectFocus: ["Typescript", "SCSS", "ReactJS"],
+  },
+  {
+    id: 2,
+    title: "Portfolio",
+    image: Portfolio,
+    link: "#",
+    github: "https://github.com/NwekeMaxwell/My-Portfolio",
+    description: "My Portfolio Website :) You are currently viewing 🤗",
+    stack: ["React", "JavaScript"],
+    projectFocus: ["Hooks", "Framer Motion", "Email JS"],
+  },
+  {
+    id: 3,
+    title: "Forkify",
+    image: Forkify,
+    link: "https://forkify2023.netlify.app",
+    github: "https://github.com/NwekeMaxwell/Forkify-app",
+    description:
+      "A model food website where you search for recipes and get a list of different food, directions to cook, and ingredients to prepare it.",
+    stack: ["JavaScript"],
+    projectFocus: [
+      "Asynchronous javaScript",
+      "Promises",
+      "Error Handling",
+      "MVC Architecture",
+      "Local Storage API",
+      "Polyfilling",
+      "Flowchart",
+    ],
+  },
+  {
+    id: 4,
+    title: "PIG GAME",
+    image: PigGame,
+    link: "https://piggame100.netlify.app",
+    github: "https://github.com/NwekeMaxwell/PIG-GAME-",
+    description:
+      "A custom model pig Game for fun. First player to earn $100 wins",
+    stack: ["JavaScript"],
+    projectFocus: ["DOM Manipulation"],
+  },
+  {
+    id: 5,
+    title: "Bankist App",
+    image: Bankist,
+    link: "https://bankist2022.netlify.app",
+    github: "https://github.com/NwekeMaxwell/Bankist",
+    description: "A model bank advert webpage",
+    stack: ["JavaScript"],
+    projectFocus: [
+      "Lazy Image loading",
+      "website consolidation",
+      "Slider",
+      "Smooth scroll",
+      "Tabbed grouping",
+    ],
+  },
+  {
+    id: 6,
+    title: "Mini-Bankist App",
+    image: MiniBankist,
+    link: "https://mini-bankist2022.netlify.app",
+    github: "https://github.com/NwekeMaxwell/mini-bankist",
+    description:
+      "A model user bank transaction webpage where you login and get loan, receive or send money to another registered user",
+    stack: ["JavaScript"],
+    projectFocus: [
+      "Internationalization of dates and numbers",
+      "FlowChart",
+      "Live Dates and Timers",
+    ],
+  },
+  {
+    id: 7,
+    title: "Mapty",
+    image: Mapty,
+    link: "https://mapty2022.netlify.app",
+    github: "https://github.com/NwekeMaxwell/Map-ty",
+    description:
+      "A workout webpage where user accesses his location in real time and records his workout sessions",
+    stack: ["JavaScript"],
+    projectFocus: [
+      "Geolocation API",
+      "Local Storage API",
+      "Object Oriented Programming(OOP)",
+      "ES6 Classes",
+      "External Libraries",
+    ],
+  },
+  {
+    id: 8,
+    title: "MaxArt",
+    image: MaxArt,
+    link: "https://maxart.netlify.app",
+    github: "https://github.com/NwekeMaxwell/My-Art-Website",
+    description: "A model art exhibition website for artists",
+    stack: ["HTML/CSS"],
+    projectFocus: ["SCSS", "CSS GRID", "Responsive WebPage"],
+  },
+  {
+    id: 9,
+    title: "Chef Max",
+    image: ChefMax,
+    link: "https://chefmax.netlify.app",
+    github: "https://github.com/NwekeMaxwell/ChefMax-Website",
+    description: "A model restaurant webpage for advert and sales",
+    stack: ["HTML/CSS"],
+    projectFocus: ["FlexBox", "SCSS", "Responsive WebPage"],
+  },
+  {
+    id: 10,
+    title: "Church App",
+    image: Church,
+    link: "https://heavenschurch.netlify.app",
+    github: "https://github.com/NwekeMaxwell/Heaven-Church-Website",
+    description:
+      "A model church website which offers users opportunity to request for prayers and book for prayer sections",
+    stack: ["HTML/CSS"],
+    projectFocus: ["CSS Animations", "Responsive Web Page"],
+  },
+  {
+    id: 11,
+    title: "Kitty Rolodex",
+    image: Kitty,
+    link: "https://kittyrolodex.netlify.app",
+    github: "https://github.com/NwekeMaxwell/React-KittyRolodex",
+    description: "A simple landing page with search functionality",
+    stack: ["React", "JavaScript"],
+    projectFocus: [
+      "Unidirectional data flow",
+      "Search functionality",
+      "APIs",
+      "Virtual DOM",
+    ],
+  },
+  {
+    id: 12,
+    title: "Guess My Age",
+    image: GuessMyAge,
+    link: "https://guessmyage2022.netlify.app",
+    github: "https://github.com/NwekeMaxwell/My-JS-Game-GuessMyAge",
+    description: "A fun guessing game for leisures",
+    stack: ["JavaScript"],
+    projectFocus: ["DOM Manipulation"],
+  },
+];
+
+export default MyProjectsData;
