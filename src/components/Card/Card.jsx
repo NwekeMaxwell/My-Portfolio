0aa112497e258543494e2f99c7661eb5a46a78d6
NwekeMaxwell <nwekemaxwell37@gmail.com>
2023-04-17 11:04:50 +0100

    update: modified and updated skills

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,27 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Card.css";
-import { Link } from "react-scroll";
-import { useContext } from "react";
-import { themeContext } from "../../Context";
+// import { Link } from "react-scroll";
+// import { useContext } from "react";
+// import { themeContext } from "../../Context";
+// const theme = useContext(themeContext);
+// const darkMode = theme.state.darkMode;
 
-const Card = ({ emoji, heading, detail }) => {
-  const theme = useContext(themeContext);
-  const darkMode = theme.state.darkMode;
+const Card = ({ percent, heading }) => {
+  const [percentValue, setPercentValue] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (percentValue >= percent) {
+        return;
+      } else {
+        setPercentValue((perc) => perc + 1);
+      }
+    }, 100);
+    return () => clearInterval(interval);
+  }, [percentValue]);
+
+  // const [percentValue, setPercentValue] = useState(0);
+  // useEffect(() => {
+  //   const interval = setInterval(() => {}, 1000);
+  //   console.log(percentValue);
+  //   return () => {
+  //     if (percentValue > percent) {
+  //       return clearInterval(interval);
+  //     } else {
+  //       setPercentValue((perc) => perc + 1);
+  //     }
+  //   };
+  // }, [percentValue]);
+
   return (
     <div className="card">
-      <img src={emoji} alt="" />
-      <span className="heading">{heading}</span>
-      <span
-        style={{ color: darkMode ? "" : "var(--secondary-dark)" }}
-        className="detail"
+      <div
+        className="circleWrap"
+        style={{
+          background: `conic-gradient(var(--primary-light) ${
+            percentValue * 3.6
+          }deg, #ededed 0deg)`,
+        }}
       >
-        {detail}
-      </span>
-      <Link spy={true} to="Projects" smooth={true}>
-        <button className="c-button">LEARN MORE</button>
-      </Link>
+        <span className="progressValue">{percentValue}%</span>
+      </div>
+      <div className="title">{heading}</div>
     </div>
   );
 };
 
+// <span
+//   style={{ color: darkMode ? "" : "var(--secondary-dark)" }}
+//   className="detail"
+// >
+//smooth scroll
+// </span>
+// <Link spy={true} to="Projects" smooth={true}>
+//   <button className="c-button">LEARN MORE</button>
+// </Link>
 export default Card;
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,75 +1,29 @@
 import React from "react";
 import "./Skills.css";
-import HeartEmoji from "../../img/heartemoji.png";
-import Glasses from "../../img/glasses.png";
-import Humble from "../../img/humble.png";
 import Card from "../Card/Card";
-import Resume from "../Maxwell's Resume.pdf";
-import { themeContext } from "../../Context";
-import { useContext } from "react";
-import { motion } from "framer-motion";
+// import Resume from "../Maxwell's Resume.pdf";
+// import { themeContext } from "../../Context";
+// import { useContext } from "react";
+// import { motion } from "framer-motion";
 
 const Skills = () => {
-  const theme = useContext(themeContext);
-  const darkMode = theme.state.darkMode;
-  const transition = { duration: 1, type: "spring" };
+  // const theme = useContext(themeContext);
+  // const darkMode = theme.state.darkMode;
+  // style={{ color: darkMode ? "var(--tertiary-light)" : "" }}
+  // const transition = { duration: 1, type: "spring" };
   return (
     <div className="skills" id="Skills">
-      {/* left side */}
-      <div className="awesome">
-        <span style={{ color: darkMode ? "var(--tertiary-light)" : "" }}>
-          My Basic
-        </span>
-        <span>Skills</span>
-        <span>
-          ReactJS, JavaScript, SASS, CSS3, HTML5, jQuery, BootStrap, TailWind.
-          <br />
-          GIT & GitHub, Jira , Figma, Adobe, PhotoShop.
-        </span>
-        <a href={Resume} download>
-          <button className="button s-button">Resume ⬇️</button>
-        </a>
-        <div className="blur s-blur"></div>
+      <h1>My Skills</h1>
+      <div className="s-container">
+        <Card heading={"JavaScript"} percent={95} />
+        <Card heading={"ReactJS"} percent={90} />
+        <Card heading={"TypeScript"} percent={70} />
+        <Card heading={"SCSS"} percent={100} />
+        <Card heading={"Python"} percent={30} />
+        <Card heading={"C"} percent={30} />
+        <Card heading={"Shell"} percent={50} />
+        <Card heading={"MySQL"} percent={40} />
       </div>
-      {/* right side */}
-      <motion.div
-        initial={{ left: "35%" }}
-        whileInView={{ left: "10rem" }}
-        transition={transition}
-        className="cards card-1"
-      >
-        <Card
-          emoji={HeartEmoji}
-          heading={"Design"}
-          detail={"Figma, Adobe Photoshop, Autodesk SketchBook"}
-        />
-      </motion.div>
-
-      <motion.div
-        initial={{ left: "-35%" }}
-        whileInView={{ left: "-8rem" }}
-        transition={transition}
-        className="cards card-2"
-      >
-        <Card
-          emoji={Glasses}
-          heading={"Developer"}
-          detail={"HTML5, CSS3, SASS, JavaScript, React"}
-        />
-      </motion.div>
-
-      <motion.div
-        initial={{ top: "18rem", left: "20rem" }}
-        whileInView={{ left: "7.5rem" }}
-        transition={transition}
-        className="cards card-3"
-      >
-        <Card
-          emoji={Humble}
-          heading={"SOCIAL"}
-          detail={"StackOverflow, LinkedIn, Twitter, Instagram, faceBook"}
-        />
-      </motion.div>
       <div className="blur s-blur-1"></div>
       <div className="blur s-blur-2"></div>
     </div>
